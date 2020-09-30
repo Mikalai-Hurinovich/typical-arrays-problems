de2a68be852327f150b4bd87caa8b27ed8ba258f
Mikalai Hurynovich <gurinovich-ns@mail.ru>
2020-10-01 00:19:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length <= 0) {
+    return 0;
+  } else {
+    return (array.sort(function(a, b){return a-b}))[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length <= 0) {
+    return 0;
+  } else {
+    return (array.sort(function(a, b){return b-a}))[0];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let sum = 0;
+  if (!array || array.length <= 0) {
+    return 0;
+  } else {
+    array.reduce(function (a, i){
+        return sum = a + i;
+    }, 0)
+}
+    return sum / array.length;
 }
